20edb498e34dfd5a7b796ac71ed3b92e8668a9b6
Said Sainz Lara <a25311056@uthermosillo.edu.mx>
2026-06-30 13:17:38 -0700

    feat: CRUD con 3 llaves foraneas, excepciones personalizadas y pruebas unitarias

--- notas.py
+++ notas.py
@@ -2,6 +2,11 @@
 git config --global user.name "Said Sainz Lara"
 
 
-
+git remote remove origin 
 git config --global user.email "a25311056@uthermosillo.edu.mx"
-git remote add origin https://github.com/SainzLaraSaid/Unidad4TSainzLara.git
+git remote add origin https://github.com/SainzLaraSaid/Unidad4SainzLara.git
+                      https://github.com/SainzLaraSaid/Unidad4TSainzLara.git
+git remote add origin https://github.com/TUUSUARIO/Unidad4TUSAPELLIDOS.git
+
+
+git remote set-url origin https://github.com/SainzLaraSaid/Unidad4TSainzLara.git
